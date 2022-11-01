ede4401bd202fde34bccffb1ac344666dd400b1f
Adedoyin <isradeoluwa@gmail.com>
2022-11-01 08:50:18 +0100

    Database and Test command configured

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,7 +6,8 @@ from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2Error
 
-from django.core.management import call_command  # Helps to call commands by name (**simulate)
+# Helps to call commands by name (**simulate)
+from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
@@ -21,7 +22,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -32,4 +33,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
