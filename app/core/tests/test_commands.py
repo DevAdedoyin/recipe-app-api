778cf119f016c0ddb9de86329cce7fefe6fdd4b3
Adedoyin <isradeoluwa@gmail.com>
2022-11-10 15:54:38 +0100

    Database Mocking Passed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,26 +2,31 @@
 Test custom Django management commands
 """
 
-from unittest.mock import patch
+from unittest.mock import patch  # Mock behavior of the database
 
+# Possible errors to get before db is ready during connection
 from psycopg2 import OperationalError as Psycopg2Error
 
 # Helps to call commands by name (**simulate)
+# helper function to call comand by the name
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
+# Check helps to chck status of the db
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
-        patched_check.return_value = True
+        patched_check.return_value = True    # When check is call return true
 
+        # execute code in wait_for_db. Check if db is ready and check if cmd is set up correctly.
         call_command('wait_for_db')
 
+        # ensures the mock value(check in our cmd) is called with the right param
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
