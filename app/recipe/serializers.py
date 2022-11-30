6241e42b401a58d1fa46f2d44ed2d8c025d6bef8
Adedoyin <isradeoluwa@gmail.com>
2022-11-30 09:34:04 +0100

    #Build Tag API: Tag Listing code and test implemented

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,6 +3,7 @@ Serializers for recipe APIs
 """
 from pyexpat import model
 from rest_framework import serializers
+from app.core.models import Tag
 
 from core.models import Recipe
 
@@ -21,3 +22,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
